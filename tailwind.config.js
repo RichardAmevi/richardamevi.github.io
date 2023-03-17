ab25061904c47a2b4290f2513690ffec26a986fa
Aderibigbe Richard <richardaderibigbe1@gmail.com>
2023-03-18 00:22:11 +0100

    Add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    raleway : ["Raleway", "sans-serif"],
+    opensans: ["Open Sans", "sans-serif"],
+
+    extend: {
+      colors: {
+        'introne': 'hsl(217, 28%, 15%)',
+        'main': 'hsl(218, 28%, 13%)',
+        'footer': 'hsl(216, 53%, 9%)',
+        'testimonials': 'hsl(219, 30%, 18%)',
+        'Cyan': 'hsl(176, 68%, 64%)',
+        'Blue': 'hsl(198, 60%, 50%)',
+        'LightRed': 'hsl(0, 100%, 63%)',
+        'White': 'hsl(0, 0%, 100%)'
+      }
+    },
   },
   plugins: [],
 }
